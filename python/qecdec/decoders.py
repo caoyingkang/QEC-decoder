67067b1f6660239aaeec21fd9ec5cb58f2c9026e
Yingkang Cao <cyngkng@gmail.com>
2025-09-21 10:24:03 -0400

    add Python wrapper for the Rust-based DMemBPDecoder and DMemOffBPDecoder; customize __getstate__ and __setstate__ methods so that the decoders are serializable

--- python/qecdec/decoders.py
+++ python/qecdec/decoders.py
@@ -1,4 +1,6 @@
 from .qecdec import BPDecoder as BPDecoder_Rust
+from .qecdec import DMemBPDecoder as DMemBPDecoder_Rust
+from .qecdec import DMemOffBPDecoder as DMemOffBPDecoder_Rust
 import numpy as np
 import pymatching
 
@@ -21,22 +23,30 @@ class Decoder:
         assert isinstance(prior, np.ndarray) and prior.ndim == 1
         assert pcm.shape[1] == prior.shape[0]
 
-        self.m: int = pcm.shape[0]
-        self.n: int = pcm.shape[1]
         self.pcm = pcm.astype(np.uint8)
         self.prior = prior.astype(np.float64)
+
+    @property
+    def num_checks(self) -> int:
+        """Number of check nodes."""
+        return self.pcm.shape[0]
+
+    @property
+    def num_variables(self) -> int:
+        """Number of variable nodes."""
+        return self.pcm.shape[1]
 
     def decode(self, syndrome: np.ndarray) -> np.ndarray:
         """
         Parameters
         ----------
             syndrome : ndarray
-                Vector of syndrome bits ∈ {0,1}, shape=(m,).
+                Vector of syndrome bits ∈ {0,1}, shape=(#checks,).
 
         Returns
         -------
             ehat : ndarray
-                Decoded error vector ∈ {0,1}, shape=(n,).
+                Decoded error vector ∈ {0,1}, shape=(#variables,).
         """
         raise NotImplementedError
 
@@ -45,12 +55,12 @@ class Decoder:
         Parameters
         ----------
             syndrome_batch : ndarray
-                Array of syndrome vectors ∈ {0,1}, shape=(batch_size,m).
+                Array of syndrome vectors ∈ {0,1}, shape=(batch_size, #checks).
 
         Returns
         -------
             ehat_batch : ndarray
-                Array of decoded error vectors ∈ {0,1}, shape=(batch_size,n).
+                Array of decoded error vectors ∈ {0,1}, shape=(batch_size, #variables).
         """
         raise NotImplementedError
 
@@ -62,22 +72,32 @@ class MWPMDecoder(Decoder):
     def __init__(self, pcm: np.ndarray, prior: np.ndarray):
         super().__init__(pcm, prior)
 
-        self.mwpm = pymatching.Matching.from_check_matrix(
+        self.decoder = pymatching.Matching.from_check_matrix(
+            self.pcm, weights=np.log((1 - self.prior) / self.prior))
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        del state["decoder"]
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        self.decoder = pymatching.Matching.from_check_matrix(
             self.pcm, weights=np.log((1 - self.prior) / self.prior))
 
     def decode(self, syndrome: np.ndarray) -> np.ndarray:
         assert isinstance(syndrome, np.ndarray)
         assert syndrome.ndim == 1
-        assert syndrome.shape[0] == self.m
+        assert syndrome.shape[0] == self.num_checks
 
-        return self.mwpm.decode(syndrome)
+        return self.decoder.decode(syndrome)
 
     def decode_batch(self, syndrome_batch: np.ndarray) -> np.ndarray:
         assert isinstance(syndrome_batch, np.ndarray)
         assert syndrome_batch.ndim == 2
-        assert syndrome_batch.shape[1] == self.m
+        assert syndrome_batch.shape[1] == self.num_checks
 
-        return self.mwpm.decode_batch(syndrome_batch)
+        return self.decoder.decode_batch(syndrome_batch)
 
 
 class BPDecoder(Decoder):
@@ -96,10 +116,10 @@ class BPDecoder(Decoder):
         Parameters
         ----------
             pcm : ndarray
-                Parity check matrix ∈ {0,1}, shape=(m,n).
+                Parity check matrix ∈ {0,1}, shape=(#checks, #variables).
 
             prior : ndarray
-                Prior error probabilities for each bit, shape=(n,).
+                Prior error probabilities for each bit, shape=(#variables,).
 
             max_iter : int
                 Max number of BP iterations.
@@ -109,29 +129,192 @@ class BPDecoder(Decoder):
                 no scaling is applied.
         """
         super().__init__(pcm, prior)
+        self.max_iter = max_iter
+        self.scaling_factor = scaling_factor
 
-        self.bp = BPDecoder_Rust(
+        self.decoder = BPDecoder_Rust(
             self.pcm, self.prior, max_iter=max_iter, scaling_factor=scaling_factor)
 
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        del state["decoder"]
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        self.decoder = BPDecoder_Rust(
+            self.pcm, self.prior, max_iter=self.max_iter, scaling_factor=self.scaling_factor)
+
     def decode(self, syndrome: np.ndarray, record_llr_history: bool = False) -> np.ndarray:
+        """
+        Parameters
+        ----------
+            syndrome : ndarray
+                Vector of syndrome bits ∈ {0,1}, shape=(#checks,).
+
+            record_llr_history : bool
+                If True, record the history of LLRs for each BP iteration.
+
+        Returns
+        -------
+            ehat : ndarray
+                Decoded error vector ∈ {0,1}, shape=(#variables,).
+        """
         assert isinstance(syndrome, np.ndarray)
         assert syndrome.ndim == 1
-        assert syndrome.shape[0] == self.m
+        assert syndrome.shape[0] == self.num_checks
 
-        return self.bp.decode(syndrome.astype(np.uint8), record_llr_history)
+        return self.decoder.decode(syndrome.astype(np.uint8), record_llr_history)
 
     def decode_batch(self, syndrome_batch: np.ndarray) -> np.ndarray:
         assert isinstance(syndrome_batch, np.ndarray)
         assert syndrome_batch.ndim == 2
-        assert syndrome_batch.shape[1] == self.m
+        assert syndrome_batch.shape[1] == self.num_checks
 
-        return self.bp.decode_batch(syndrome_batch.astype(np.uint8))
+        return self.decoder.decode_batch(syndrome_batch.astype(np.uint8))
 
     def get_llr_history(self) -> np.ndarray:
-        return self.bp.get_llr_history()
+        return self.decoder.get_llr_history()
+
+
+class DMemBPDecoder(Decoder):
+    """Distributed Memory Belief Propagation decoder. This class is a wrapper for the Rust implementation.
+    """
+
+    def __init__(
+        self,
+        pcm: np.ndarray,
+        prior: np.ndarray,
+        *,
+        gamma: np.ndarray,
+        max_iter: int,
+        scaling_factor: float | None = None,
+    ):
+        """
+        Parameters
+        ----------
+            pcm : ndarray
+                Parity check matrix ∈ {0,1}, shape=(#checks, #variables).
+
+            prior : ndarray
+                Prior error probabilities for each bit, shape=(#variables,).
+
+            gamma : ndarray
+                Memory strength for each variable node, shape=(#variables,).
+
+            max_iter : int
+                Max number of BP iterations.
+
+            scaling_factor : float or None
+                Scaling factor (a.k.a. normalization factor) for the BP messages. If None, 
+                no scaling is applied.
+        """
+        super().__init__(pcm, prior)
+        self.gamma = gamma
+        self.max_iter = max_iter
+        self.scaling_factor = scaling_factor
+
+        self.decoder = DMemBPDecoder_Rust(
+            self.pcm, self.prior, gamma=gamma, max_iter=max_iter, scaling_factor=scaling_factor)
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        del state["decoder"]
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        self.decoder = DMemBPDecoder_Rust(
+            self.pcm, self.prior, gamma=self.gamma, max_iter=self.max_iter, scaling_factor=self.scaling_factor)
+
+    def decode(self, syndrome: np.ndarray) -> np.ndarray:
+        assert isinstance(syndrome, np.ndarray)
+        assert syndrome.ndim == 1
+        assert syndrome.shape[0] == self.num_checks
+
+        return self.decoder.decode(syndrome.astype(np.uint8))
+
+    def decode_batch(self, syndrome_batch: np.ndarray) -> np.ndarray:
+        assert isinstance(syndrome_batch, np.ndarray)
+        assert syndrome_batch.ndim == 2
+        assert syndrome_batch.shape[1] == self.num_checks
+
+        return self.decoder.decode_batch(syndrome_batch.astype(np.uint8))
+
+
+class DMemOffBPDecoder(Decoder):
+    """Distributed Memory Offset Belief Propagation decoder. This class is a wrapper for the Rust implementation.
+    """
+
+    def __init__(
+        self,
+        pcm: np.ndarray,
+        prior: np.ndarray,
+        *,
+        gamma: np.ndarray,
+        offset: list[list[float]],
+        max_iter: int,
+        scaling_factor: float | None = None,
+    ):
+        """
+        Parameters
+        ----------
+            pcm : ndarray
+                Parity check matrix ∈ {0,1}, shape=(#checks, #variables).
+
+            prior : ndarray
+                Prior error probabilities for each bit, shape=(#variables,).
+
+            gamma : ndarray
+                Memory strength for each variable node, shape=(#variables,).
+
+            offset : list[list[float]]
+                Offset parameters.
+
+            max_iter : int
+                Max number of BP iterations.
+
+            scaling_factor : float or None
+                Scaling factor (a.k.a. normalization factor) for the BP messages. If None, 
+                no scaling is applied.
+        """
+        super().__init__(pcm, prior)
+        self.gamma = gamma
+        self.offset = offset
+        self.max_iter = max_iter
+        self.scaling_factor = scaling_factor
+
+        self.decoder = DMemOffBPDecoder_Rust(
+            self.pcm, self.prior, gamma=gamma, offset=offset, max_iter=max_iter, scaling_factor=scaling_factor)
+
+    def __getstate__(self):
+        state = self.__dict__.copy()
+        del state["decoder"]
+        return state
+
+    def __setstate__(self, state):
+        self.__dict__.update(state)
+        self.decoder = DMemOffBPDecoder_Rust(
+            self.pcm, self.prior, gamma=self.gamma, offset=self.offset, max_iter=self.max_iter, scaling_factor=self.scaling_factor)
+
+    def decode(self, syndrome: np.ndarray) -> np.ndarray:
+        assert isinstance(syndrome, np.ndarray)
+        assert syndrome.ndim == 1
+        assert syndrome.shape[0] == self.num_checks
+
+        return self.decoder.decode(syndrome.astype(np.uint8))
+
+    def decode_batch(self, syndrome_batch: np.ndarray) -> np.ndarray:
+        assert isinstance(syndrome_batch, np.ndarray)
+        assert syndrome_batch.ndim == 2
+        assert syndrome_batch.shape[1] == self.num_checks
+
+        return self.decoder.decode_batch(syndrome_batch.astype(np.uint8))
 
 
 __all__ = [
     "MWPMDecoder",
     "BPDecoder",
+    "DMemBPDecoder",
+    "DMemOffBPDecoder",
 ]
