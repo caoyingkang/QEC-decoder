cb6fcca249359c85fbed23f8c2736417da7dabaf
Yingkang Cao <cyngkng@gmail.com>
2025-09-30 16:46:32 -0400

    fix a small bug in building datasets when train_all_wt1_errors or train_all_wt2_errors is set to False; add save/load functionailty to DecodingDataset

--- pytorch/train_utils.py
+++ pytorch/train_utils.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from typing import Optional
+from pathlib import Path
 from qecdec.experiments import MemoryExperiment
 from qecdec.decoders import BPDecoder
 from learned_decoders import LearnedBPBase, DecodingLoss, DecodingMetric
@@ -24,30 +25,54 @@ class DecodingDataset(Dataset):
 
     def __init__(
         self,
-        syndromes: np.ndarray,
-        observables: np.ndarray,
+        syndromes: np.ndarray | torch.Tensor,
+        observables: np.ndarray | torch.Tensor,
     ):
         """
         Parameters
         ----------
-            syndromes : np.ndarray
+            syndromes : np.ndarray | torch.Tensor
                 Syndrome bits ∈ {0,1}, shape=(num_shots, m), integer or bool
 
-            observables : np.ndarray
+            observables : np.ndarray | torch.Tensor
                 Observable bits ∈ {0,1}, shape=(num_shots, k), integer or bool
         """
-        assert isinstance(syndromes, np.ndarray)
-        assert isinstance(observables, np.ndarray)
-        assert np.issubdtype(syndromes.dtype, np.integer) or \
-            np.issubdtype(syndromes.dtype, np.bool_)
-        assert np.issubdtype(observables.dtype, np.integer) or \
-            np.issubdtype(observables.dtype, np.bool_)
+        assert isinstance(syndromes, np.ndarray) or isinstance(
+            syndromes, torch.Tensor)
+        assert isinstance(observables, np.ndarray) or isinstance(
+            observables, torch.Tensor)
         assert syndromes.ndim == 2
         assert observables.ndim == 2
         assert syndromes.shape[0] == observables.shape[0]
 
         self.syndromes = torch.as_tensor(syndromes, dtype=INT_DTYPE)
         self.observables = torch.as_tensor(observables, dtype=INT_DTYPE)
+
+    @classmethod
+    def load_from_file(cls, file: str | Path):
+        """
+        Load the dataset from a file.
+        """
+        if isinstance(file, str):
+            file = Path(file)
+        if not file.exists():
+            raise FileNotFoundError(f"File {file} does not exist")
+
+        syndromes, observables = torch.load(file)
+        return cls(syndromes, observables)
+
+    def save_to_file(self, file: str | Path, overwrite_ok: bool = False):
+        """
+        Save the dataset to a file.
+        """
+        if isinstance(file, str):
+            file = Path(file)
+        if file.exists() and not overwrite_ok:
+            raise FileExistsError(
+                f"File {file} already exists, and overwrite_ok is set to False")
+
+        file.parent.mkdir(parents=True, exist_ok=True)
+        torch.save((self.syndromes, self.observables), file)
 
     def __len__(self):
         return len(self.syndromes)
@@ -156,13 +181,13 @@ def build_datasets_version_1(
     s, o, _ = sampler.sample(train_shots + val_shots)
     s = s.astype(np.int32)
     o = o.astype(np.int32)
-    train_syndromes_sampled, train_observables_sampled = filter(
+    train_syndromes, train_observables = filter(
         s[:train_shots], o[:train_shots])
     val_syndromes, val_observables = filter(
         s[train_shots:], o[train_shots:])
     if verbose:
         print(
-            f"Added {len(train_syndromes_sampled)} samples to the training dataset.")
+            f"Added {len(train_syndromes)} samples to the training dataset.")
         print(
             f"Added {len(val_syndromes)} samples to the validation dataset.")
 
@@ -172,6 +197,10 @@ def build_datasets_version_1(
         errors = np.eye(n, dtype=np.int32)
         train_syndromes_from_wt1_errors, train_observables_from_wt1_errors = filter(
             (errors @ expmt.chkmat.T) % 2, (errors @ expmt.obsmat.T) % 2)
+        train_syndromes = np.concatenate(
+            [train_syndromes, train_syndromes_from_wt1_errors])
+        train_observables = np.concatenate(
+            [train_observables, train_observables_from_wt1_errors])
         if verbose:
             print(
                 f"Added {len(train_syndromes_from_wt1_errors)} samples to the training dataset.")
@@ -184,16 +213,14 @@ def build_datasets_version_1(
             errors[row, cols] = 1
         train_syndromes_from_wt2_errors, train_observables_from_wt2_errors = filter(
             (errors @ expmt.chkmat.T) % 2, (errors @ expmt.obsmat.T) % 2)
+        train_syndromes = np.concatenate(
+            [train_syndromes, train_syndromes_from_wt2_errors])
+        train_observables = np.concatenate(
+            [train_observables, train_observables_from_wt2_errors])
         if verbose:
             print(
                 f"Added {len(train_syndromes_from_wt2_errors)} samples to the training dataset.")
 
-    train_syndromes = np.concatenate([train_syndromes_sampled,
-                                      train_syndromes_from_wt1_errors,
-                                      train_syndromes_from_wt2_errors])
-    train_observables = np.concatenate([train_observables_sampled,
-                                        train_observables_from_wt1_errors,
-                                        train_observables_from_wt2_errors])
     train_dataset = DecodingDataset(train_syndromes, train_observables)
     val_dataset = DecodingDataset(val_syndromes, val_observables)
 
@@ -210,6 +237,8 @@ def build_datasets_version_2(
     train_shots: int,
     val_shots: int,
     seed: Optional[int] = None,
+    train_all_wt1_errors: bool = True,
+    train_all_wt2_errors: bool = True,
     verbose: bool = True,
 ) -> tuple[DecodingDataset, DecodingDataset]:
     """
@@ -226,6 +255,12 @@ def build_datasets_version_2(
 
         seed : int | None
             Random seed used for sampling
+
+        train_all_wt1_errors : bool
+            Whether to include all weight-1 errors in `train_dataset`
+
+        train_all_wt2_errors : bool
+            Whether to include all weight-2 errors in `train_dataset`
 
         verbose : bool
             Whether to print verbose output
@@ -264,47 +299,51 @@ def build_datasets_version_2(
     sampler = expmt.dem.compile_sampler(seed=seed)
     syn, obs, _ = sampler.sample(train_shots + val_shots)
     syn, obs = syn.astype(np.int32), obs.astype(np.int32)
-    train_syndromes_sampled, train_observables_sampled = filter(
+    train_syndromes, train_observables = filter(
         syn[:train_shots], obs[:train_shots], remove_easy_instances=True)
     val_syndromes, val_observables = filter(
         syn[train_shots:], obs[train_shots:], remove_easy_instances=True)
     if verbose:
         print(
-            f"Added {len(train_syndromes_sampled)} samples to the training dataset.")
+            f"Added {len(train_syndromes)} samples to the training dataset.")
         print(
             f"Added {len(val_syndromes)} samples to the validation dataset.")
 
     # =============================== weight-1 errors ===============================
-    if verbose:
-        print("Generating all weight-1 errors...")
-    errors = np.eye(n, dtype=np.int32)
-    syn, obs = (errors @ expmt.chkmat.T) % 2, (errors @ expmt.obsmat.T) % 2
-    train_syndromes_from_wt1_errors, train_observables_from_wt1_errors = filter(
-        syn, obs, remove_easy_instances=False)
-    if verbose:
-        print(
-            f"Added {len(train_syndromes_from_wt1_errors)} samples to the training dataset.")
+    if train_all_wt1_errors:
+        if verbose:
+            print("Generating all weight-1 errors...")
+        errors = np.eye(n, dtype=np.int32)
+        syn, obs = (errors @ expmt.chkmat.T) % 2, (errors @ expmt.obsmat.T) % 2
+        train_syndromes_from_wt1_errors, train_observables_from_wt1_errors = filter(
+            syn, obs, remove_easy_instances=False)
+        train_syndromes = np.concatenate(
+            [train_syndromes, train_syndromes_from_wt1_errors])
+        train_observables = np.concatenate(
+            [train_observables, train_observables_from_wt1_errors])
+        if verbose:
+            print(
+                f"Added {len(train_syndromes_from_wt1_errors)} samples to the training dataset.")
 
     # =============================== weight-2 errors ===============================
-    if verbose:
-        print("Generating all weight-2 errors...")
-    errors = np.zeros(((n * (n - 1)) // 2, n), dtype=np.int32)
-    for row, cols in enumerate(combinations(range(n), 2)):
-        errors[row, cols] = 1
-    syn, obs = (errors @ expmt.chkmat.T) % 2, (errors @ expmt.obsmat.T) % 2
-    train_syndromes_from_wt2_errors, train_observables_from_wt2_errors = filter(
-        syn, obs, remove_easy_instances=True)
-    if verbose:
-        print(
-            f"Added {len(train_syndromes_from_wt2_errors)} samples to the training dataset.")
+    if train_all_wt2_errors:
+        if verbose:
+            print("Generating all weight-2 errors...")
+        errors = np.zeros(((n * (n - 1)) // 2, n), dtype=np.int32)
+        for row, cols in enumerate(combinations(range(n), 2)):
+            errors[row, cols] = 1
+        syn, obs = (errors @ expmt.chkmat.T) % 2, (errors @ expmt.obsmat.T) % 2
+        train_syndromes_from_wt2_errors, train_observables_from_wt2_errors = filter(
+            syn, obs, remove_easy_instances=True)
+        train_syndromes = np.concatenate(
+            [train_syndromes, train_syndromes_from_wt2_errors])
+        train_observables = np.concatenate(
+            [train_observables, train_observables_from_wt2_errors])
+        if verbose:
+            print(
+                f"Added {len(train_syndromes_from_wt2_errors)} samples to the training dataset.")
 
     # =============================== collect all instances ===============================
-    train_syndromes = np.concatenate([train_syndromes_sampled,
-                                      train_syndromes_from_wt1_errors,
-                                      train_syndromes_from_wt2_errors])
-    train_observables = np.concatenate([train_observables_sampled,
-                                        train_observables_from_wt1_errors,
-                                        train_observables_from_wt2_errors])
     train_dataset = DecodingDataset(train_syndromes, train_observables)
     val_dataset = DecodingDataset(val_syndromes, val_observables)
 
