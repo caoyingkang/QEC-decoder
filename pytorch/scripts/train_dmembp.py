8106b1a4431fe0bc893d8113ad39e5fee4a58ada
caoyingkang <cyngkng@gmail.com>
2026-03-06 15:00:41 +0000

    Add ModelCheckpoint callback

--- pytorch/scripts/train_dmembp.py
+++ pytorch/scripts/train_dmembp.py
@@ -3,12 +3,14 @@ import os
 from pathlib import Path
 
 import lightning as L
-from lightning.pytorch.callbacks import EarlyStopping
+from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
 from lightning.pytorch.loggers import TensorBoardLogger
 from omegaconf import OmegaConf, DictConfig
 from qecdec import RotatedSurfaceCode_Memory
 
 _PYTORCH_ROOT = Path(__file__).resolve().parent.parent
+_DATASETS_ROOT = _PYTORCH_ROOT / "datasets"
+_RUNS_ROOT = _PYTORCH_ROOT / "runs"
 _DEFAULT_CONFIG_PATH = _PYTORCH_ROOT / "configs" / (Path(__file__).stem + ".yaml")
 
 
@@ -25,13 +27,24 @@ def load_config() -> DictConfig:
 def get_data_dir(qec_cfg: DictConfig) -> Path:
     d = qec_cfg.d
     rounds = qec_cfg.rounds
-    return Path(__file__).resolve().parent.parent / "datasets" / "rotated_surface_code_memory_Z" / f"d={d}_rounds={rounds}"
+    basis = qec_cfg.basis
+    return _DATASETS_ROOT / "rotated_surface_code_memory" / f"d={d}_rounds={rounds}_basis={basis}"
+
+
+def get_run_dir(cfg: DictConfig) -> Path:
+    d = cfg.qec.d
+    rounds = cfg.qec.rounds
+    basis = cfg.qec.basis
+    return _RUNS_ROOT / "rotated_surface_code_memory" / f"d={d}_rounds={rounds}_basis={basis}" / cfg.model.name
 
 
 def main():
     cfg = load_config()
     print(">>>>>> Config:")
     print(OmegaConf.to_yaml(cfg))
+
+    data_dir = get_data_dir(cfg.qec)
+    run_dir = get_run_dir(cfg)
 
     expmt = RotatedSurfaceCode_Memory(
         d=cfg.qec.d,
@@ -50,18 +63,26 @@ def main():
         optim_cfg=cfg.optim,
     )
     datamodule = DecodingDataModule(
-        get_data_dir(cfg.qec),
+        data_dir,
         batch_size=cfg.data.batch_size,
         num_workers=cfg.data.num_workers,
     )
-    early_stopping = EarlyStopping(
+    early_stopping_callback = EarlyStopping(
         monitor="val_loss",
         min_delta=cfg.early_stopping.min_delta,
         patience=cfg.early_stopping.patience,
         verbose=True,
         mode="min",
     )
-    epoch_summary = EpochSummary()
+    epoch_summary_callback = EpochSummary()
+    model_checkpoint_callback = ModelCheckpoint(
+        dirpath=run_dir,
+        filename="best_model",
+        monitor="val_loss",
+        save_last=True,
+        save_top_k=1,
+        mode="min",
+    )
     tb_logger = TensorBoardLogger(
         save_dir="",
         log_graph=cfg.tb_logger.log_graph,
@@ -70,7 +91,11 @@ def main():
         accelerator=cfg.trainer.accelerator,
         max_epochs=cfg.trainer.max_epochs,
         enable_progress_bar=cfg.trainer.enable_progress_bar,
-        callbacks=[early_stopping, epoch_summary],
+        callbacks=[
+            early_stopping_callback,
+            epoch_summary_callback,
+            model_checkpoint_callback,
+        ],
         logger=tb_logger,
     )
     trainer.fit(decoder, datamodule=datamodule)
