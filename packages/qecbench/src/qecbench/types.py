cc7e663b5b603a45e91720e7d9ce076d5a2ee751
Yingkang Cao <cyngkng@gmail.com>
2026-03-24 20:44:43 -0400

    update uv toml files

--- packages/qecbench/src/qecbench/types.py
+++
@@ -1,7 +0,0 @@
-from typing import TypeAlias
-
-import numpy as np
-
-Int1DArray: TypeAlias = np.ndarray[tuple[int], np.dtype[np.int64]]
-Bool1DArray: TypeAlias = np.ndarray[tuple[int], np.dtype[np.bool_]]
-Bit2DArray: TypeAlias = np.ndarray[tuple[int, int], np.dtype[np.uint8]]
