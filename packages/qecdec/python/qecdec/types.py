4570a561a8005713471f1df7e8025036efb13111
Yingkang Cao <cyngkng@gmail.com>
2026-03-25 14:22:40 -0400

    Add type stubs for the qecdec Rust extension

---
+++ packages/qecdec/python/qecdec/types.py
@@ -0,0 +1,11 @@
+from typing import TypeAlias
+
+import numpy as np
+
+
+Bit1DArray: TypeAlias = np.ndarray[tuple[int], np.dtype[np.uint8]]
+Bit2DArray: TypeAlias = np.ndarray[tuple[int, int], np.dtype[np.uint8]]
+Bool1DArray: TypeAlias = np.ndarray[tuple[int], np.dtype[np.bool_]]
+Float1DArray: TypeAlias = np.ndarray[tuple[int], np.dtype[np.float64]]
+Float2DArray: TypeAlias = np.ndarray[tuple[int, int], np.dtype[np.float64]]
+Int1DArray: TypeAlias = np.ndarray[tuple[int], np.dtype[np.int64]]
