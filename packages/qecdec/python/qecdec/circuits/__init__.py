fc1feb503eff8f9a2c4d53ec5aa4f3ae31044922
Yingkang Cao <cyngkng@gmail.com>
2026-05-21 02:04:51 -0400

    export GrossCode_Circuit

--- packages/qecdec/python/qecdec/circuits/__init__.py
+++ packages/qecdec/python/qecdec/circuits/__init__.py
@@ -1,14 +1,16 @@
 """Module for QEC circuits."""
 
 from .base import CIRCUITS_REGISTRY, QECCircuit
+from .gross_code_circuit import GrossCode_Circuit
 from .hex_color_code_phenom import HexColorCode_Phenom
 from .repetition_code_circuit import RepetitionCode_Circuit
 from .rotated_surface_code_circuit import RotatedSurfaceCode_Circuit
 
 __all__ = [
-    CIRCUITS_REGISTRY,
-    QECCircuit,
-    HexColorCode_Phenom,
-    RepetitionCode_Circuit,
-    RotatedSurfaceCode_Circuit,
+    "CIRCUITS_REGISTRY",
+    "QECCircuit",
+    "GrossCode_Circuit",
+    "HexColorCode_Phenom",
+    "RepetitionCode_Circuit",
+    "RotatedSurfaceCode_Circuit",
 ]
